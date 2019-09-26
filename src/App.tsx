e66eb3c2a691b28b552079e5d488b4a552e1e3db
byseop <byseop@gmail.com>
2019-09-26 16:21:42 +0900

    상태관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import Greetings from './Greetings';
+import MyForm from './MyForm';
+import Counter from './Counter';
+import ReducerSample from './ReducerSample';
 
 const App: React.FC = () => {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`)
+  const onSubmit = (form: { name: string, description: string }) => {
+    console.log(form);
   }
   return (
-    <div className="App">
-      <Greetings name="Hello" onClick={onClick} />
-    </div>
+    <>
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+      <ReducerSample />
+    </>
   );
 }
 
