979e545b76ef58e18d99814d5ad9f7fc34dcb64e
byseop <byseop@gmail.com>
2019-09-26 14:50:37 +0900

    타입스크립트를 이용한 리액트1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Greetings from './Greetings';
 
 const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greetings name="Hello" onClick={onClick} />
     </div>
   );
 }
